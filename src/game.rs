f1b282fcc8088bfdb5f35ffa26332eb4474f34b2
Nicholas Sixbury <nico6bury@ksu.edu>
2023-07-03 20:20:15 -0500

    Linked game file with enums.

--- src/game.rs
+++ src/game.rs
@@ -1,5 +1,13 @@
+use grid::Grid;
+
+use crate::enums::PieceType;
+mod enums;
 
 pub struct Board {
 	/// the grid that represents the game board
 	pub board:Grid<String>,
 }//end struct Board
+
+pub struct BoardPiece {
+	pub piece_type: PieceType,
+}//end struct BoardPiece
