4f958ab8e26fcf454990bd83b1b1bd4ee5ec0e2b
Rachel Pang <rlpang6@gmail.com>
2023-07-13 00:13:07 -0500

    changed size of availible trees text display

--- src/gui.rs
+++ src/gui.rs
@@ -301,7 +301,7 @@ impl GUI {
 		self.main_window.add(&self.buying_trees_choice);
 
 		self.available_trees_display = TextDisplay::default()
-			.with_size(135,100)
+			.with_size(135,135)
 			.below_of(&self.buying_trees_choice,100)
 			.with_label("Available Area")
 			.with_align(Align::LeftTop);
