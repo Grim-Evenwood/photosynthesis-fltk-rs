fe0c4d2a5b14d1270a23ed61b9f7926901c5f70b
Nicholas Sixbury <nico6bury@ksu.edu>
2023-07-23 02:22:53 -0500

    Used magic to turn FlexGrid into a proper Widget. Reorganized gui a small bit to account for this change.

--- src/gui.rs
+++ src/gui.rs
@@ -1,9 +1,13 @@
-use fltk::{window::Window, app::{App, Receiver, Sender, self}, prelude::{WidgetExt, GroupExt, WidgetBase, MenuExt, DisplayExt, ImageExt}, enums::{Color, Align, Shortcut}, button::Button, group::{Flex, self}, menu::{SysMenuBar, self, Choice}, text::{TextEditor, TextBuffer, self, TextDisplay}, image::{PngImage, JpegImage}, frame::Frame};
+use fltk::{window::Window, app::{App, Receiver, Sender, self}, prelude::{WidgetExt, GroupExt, WidgetBase, MenuExt, DisplayExt, ImageExt}, enums::{Color, Align, Shortcut}, button::Button, group::{Flex, self}, menu::{SysMenuBar, self, Choice}, text::{TextEditor, TextBuffer, self, TextDisplay}, image::{PngImage, JpegImage}, frame::Frame, widget_extends};
 use grid::Grid;
 use fltk_theme::WidgetScheme;
 use fltk_theme::SchemeType;
 use fltk_theme::widget_themes;
 use crate::{Board, game::{Fertility, Tree}};
+
+use self::gui_utils::{get_default_win_width, get_max_grid_button_width, get_default_grid_width, FlexGrid, get_default_grid_padding, get_default_grid_height, get_default_txt_height, get_default_txt_width, get_default_txt_padding, get_default_menu_height, get_default_win_height, get_max_grid_button_height};
+
+mod gui_utils;
 
 #[warn(missing_docs)]
 pub struct GUI {
@@ -317,8 +321,8 @@ impl GUI {
 		flex.fill_flex(&test_grid);
 
 		// make the flex actually show up in program
-		self.main_window.add(&flex.outer_flex);
-		flex.outer_flex.recalc();
+		self.main_window.add(&*flex);
+		flex.recalc();
 	}//end button_grid_test(self)
 
 	/// # initialize_menu(&mut self)
@@ -395,7 +399,7 @@ impl GUI {
 		let mut board: Grid<Button> = Grid::new(board_state.board.rows(),board_state.board.cols());
 		self.board_flex_grid = FlexGrid::default();
 		self.board_flex_grid.clear_inner_flexes();
-		self.board_flex_grid.outer_flex.clear();
+		self.board_flex_grid.clear();
 		
 		// intiailize board 
 		for row in 0..board.rows(){
@@ -433,9 +437,9 @@ impl GUI {
 		self.board_flex_grid.fill_flex(&board);
 
 		// make flex show up 
-		self.board_flex_grid.outer_flex.set_pos(get_default_win_width() - get_default_grid_width() - 100, 175);
-		self.main_window.add(&self.board_flex_grid.outer_flex);
-		self.board_flex_grid.outer_flex.recalc();
+		self.board_flex_grid.set_pos(get_default_win_width() - get_default_grid_width() - 100, 175);
+		self.main_window.add(&*self.board_flex_grid);
+		self.board_flex_grid.recalc();
 	}//end initialize_board(&mut self, board)
 
 	/// # initialize_tree_lists(self, to_buy, available)
@@ -536,97 +540,3 @@ impl GUI {
 		self.availible_trees.set_text(&available_text);
 	}//end update_tree_lists(self, to_buy, available)
 }//end impl for GUI
-
-fn get_default_win_width() -> i32 {1000}
-fn get_default_win_height() -> i32 {700}
-fn get_default_menu_height() -> i32 {30}
-fn get_default_tab_padding() -> i32 {0}
-fn get_default_grid_width() -> i32 {get_default_win_width() - 400}
-fn get_default_grid_height() -> i32 {get_default_win_height()-get_default_menu_height() - get_default_tab_padding() - 225}
-fn get_max_grid_button_width() -> i32 {30}
-fn get_max_grid_button_height() -> i32 {15}
-fn get_default_txt_width() -> i32 {(get_default_win_width() / 6) - (get_default_txt_padding() * 3 / 2)}
-fn get_default_txt_height() -> i32 {25}
-fn get_default_txt_padding() -> i32 {10}
-fn get_default_grid_padding() -> i32 {100}
-
-/// # FlexGrid
-/// 
-/// This struct is meant to be a sort of wrapper around a bunch of buttons and nested flexes in order to mimic a grid of buttons.
-pub struct FlexGrid {
-	/// # buttons
-	/// The 2d array of buttons filling the grid
-	pub buttons: Grid<Button>,
-	/// # outer_flex
-	/// The flex containing the flex containing the buttons
-	pub outer_flex: Flex,
-	/// # inner_flexes
-	/// the flexes contained within the inner flex
-	pub inner_flexes: Vec<Flex>,
-}//end struct FlexGrid
-
-impl FlexGrid {
-	/// # default()
-	/// 
-	/// constructs the empty FlexGrid
-	pub fn default() -> FlexGrid {
-		let new_outer_flex = Flex::new(0, get_default_menu_height() + get_default_tab_padding(), get_default_grid_width(), get_default_grid_height(), None);
-		new_outer_flex.end();
-		FlexGrid {
-			buttons: Grid::new(0,0),
-			outer_flex: new_outer_flex,
-			inner_flexes: Vec::new(),
-		}//end struct construction
-	}//end new()
-
-	/// # clear_inner_flexes
-	/// 
-	/// clears the children of this struct. should hopefully work
-	pub fn clear_inner_flexes(&mut self) {
-		self.outer_flex.clear();
-		self.inner_flexes.clear();
-		self.buttons.clear();
-	}//end clear_inner_flexes(&mut self)
-
-	/// #initialize_flex(self, grid)]
-	/// 
-	/// Sets up the flex-boxes like a grid
-	pub fn initialize_flex(&mut self, rows:usize, cols:usize) {
-		// set outer flex to be have rows of elements
-		self.outer_flex.set_type(group::FlexType::Column);
-		self.outer_flex.set_align(Align::LeftTop);
-		for _row_index in 0..rows {
-			let inner_flex_x = 0;//self.outer_flex.x();
-			let inner_flex_y = self.outer_flex.y() + (self.outer_flex.width() / cols as i32);
-			let inner_flex_w = get_default_grid_width() / cols as i32;
-			let inner_flex_h = get_default_grid_height() / rows as i32;
-			let mut inner_flex = Flex::new(inner_flex_x,inner_flex_y,inner_flex_w,inner_flex_h,None);
-			inner_flex.set_type(group::FlexType::Row);
-			// make flex show up
-			self.outer_flex.add(&inner_flex);
-			// save flex to struct
-			self.inner_flexes.push(inner_flex);
-		}//end adding inner flexes
-		// println!("{} inner flexes", self.inner_flexes.len());
-		// println!("inner flex x:{}", self.inner_flexes.first().unwrap().x());
-	}//end initialize_flex(self, grid)
-
-	/// # fill_flex(self, buttons)
-	/// fills up the flex with buttons such that the buttons will show up in the flex looking like a grid
-	/// 
-	/// It should be noted that this function should expect to receive things in the order of col, rows
-	pub fn fill_flex(&mut self, buttons:&Grid<Button>) {
-		for row_idx in 0..buttons.rows() {
-			let this_inner_flex = self.inner_flexes.get_mut(row_idx).unwrap();
-			// loop over the current row of buttons
-			for button in buttons.iter_row(row_idx) {
-				if !button.was_deleted() {
-					this_inner_flex.add(button);
-				}//end if button wasn't deleted
-				else {println!("button was deleted, row {}", row_idx);}
-			}//end adding each button in row to inner flex
-			this_inner_flex.end();
-		}//end looping over each inner flex and adding buttons
-		self.outer_flex.end();
-	}//end fill_flex
-}//end impl for FlexGrid
